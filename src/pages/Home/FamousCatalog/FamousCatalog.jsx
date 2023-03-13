df77606ba91d5b1a8e11cc7ff627b2efdd37ea13
husanovjamshid <husanovjamshid11@gmail.com>
2023-03-13 14:45:49 +0500

    add about page

--- src/pages/Home/FamousCatalog/FamousCatalog.jsx
+++ src/pages/Home/FamousCatalog/FamousCatalog.jsx
@@ -12,12 +12,12 @@ export const FamousCatalog = () => {
 		<div className='famous-category'>
 			<div className='containers'>
 				<h3 className='famous-title'>Популярное в каталоге</h3>
-				<div className='row'>
+				<div className='row g-4'>
 					<div className='col-12 col-sm-12 col-md-6 col-lg-4'>
 						<div className='catalog-card'>
 							<div className='position-relative'>
 								<img
-									src='https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp'
+									src='https://domiktut.ru/wp-content/uploads/2022/07/20200827_113924-480x320.jpg'
 									className='card-img-top'
 									alt='Fissure in Sandstone'
 								/>
@@ -53,40 +53,197 @@ export const FamousCatalog = () => {
 						</div>
 					</div>
 					<div className='col-12 col-sm-12 col-md-6 col-lg-4'>
-						<div className='card'>
-							<img
-								src='https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp'
-								className='card-img-top'
-								alt='Fissure in Sandstone'
-							/>
+						<div className='catalog-card'>
+							<div className='position-relative'>
+								<img
+									src='https://domiktut.ru/wp-content/uploads/2022/07/20200827_113924-480x320.jpg'
+									className='card-img-top'
+									alt='Fissure in Sandstone'
+								/>
+								<div className='favorite'>
+									<img src={heart} alt='' />
+								</div>
+								<div className='number-of-poeple'>
+									<img src={people} alt='' />
+									до 12
+								</div>
+							</div>
 							<div className='card-body'>
-								<h5 className='card-catalog-title'>Новая сосновка</h5>
-								<p className='card-text'>
-									Some quick example text to build on the card title and make up the bulk
-									of the card's content.
+								<h5 className='card-catalog-title'>Новая сосновка</h5>{' '}
+								<p className='card-catalog-text'>
+									<img src={bed} alt='' /> 25 спальных мест
 								</p>
-								<a href='#!' className='btn btn-primary'>
-									Button
-								</a>
+								<p className='card-catalog-text'>
+									<img src={entertaiment} alt='' /> Настольный теннис
+								</p>
+								<p className='card-catalog-text'>
+									<img src={stroke} alt='' /> Бассейн
+								</p>
+								<p className='card-catalog-text'>
+									<img src={basseyn} alt='' /> Сауна{' '}
+								</p>
+								<div className='d-flex align-items-center justify-content-between mt-4'>
+									<p className='card-catalog-price m-0'>от 10 000 ₽ / сутки </p>
+									<a className='card-catalog-link' href='#'>
+										Подробнее
+									</a>
+								</div>
 							</div>
 						</div>
 					</div>
 					<div className='col-12 col-sm-12 col-md-6 col-lg-4'>
-						<div className='card'>
-							<img
-								src='https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp'
-								className='card-img-top'
-								alt='Fissure in Sandstone'
-							/>
+						<div className='catalog-card'>
+							<div className='position-relative'>
+								<img
+									src='https://domiktut.ru/wp-content/uploads/2022/07/20200827_113924-480x320.jpg'
+									className='card-img-top'
+									alt='Fissure in Sandstone'
+								/>
+								<div className='favorite'>
+									<img src={heart} alt='' />
+								</div>
+								<div className='number-of-poeple'>
+									<img src={people} alt='' />
+									до 12
+								</div>
+							</div>
 							<div className='card-body'>
-								<h5 className='card-catalog-title'>Новая сосновка</h5>
-								<p className='card-text'>
-									Some quick example text to build on the card title and make up the bulk
-									of the card's content.
+								<h5 className='card-catalog-title'>Новая сосновка</h5>{' '}
+								<p className='card-catalog-text'>
+									<img src={bed} alt='' /> 25 спальных мест
 								</p>
-								<a href='#!' className='btn btn-primary'>
-									Button
-								</a>
+								<p className='card-catalog-text'>
+									<img src={entertaiment} alt='' /> Настольный теннис
+								</p>
+								<p className='card-catalog-text'>
+									<img src={stroke} alt='' /> Бассейн
+								</p>
+								<p className='card-catalog-text'>
+									<img src={basseyn} alt='' /> Сауна{' '}
+								</p>
+								<div className='d-flex align-items-center justify-content-between mt-4'>
+									<p className='card-catalog-price m-0'>от 10 000 ₽ / сутки </p>
+									<a className='card-catalog-link' href='#'>
+										Подробнее
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className='col-12 col-sm-12 col-md-6 col-lg-4'>
+						<div className='catalog-card'>
+							<div className='position-relative'>
+								<img
+									src='https://domiktut.ru/wp-content/uploads/2022/07/20200827_113924-480x320.jpg'
+									className='card-img-top'
+									alt='Fissure in Sandstone'
+								/>
+								<div className='favorite'>
+									<img src={heart} alt='' />
+								</div>
+								<div className='number-of-poeple'>
+									<img src={people} alt='' />
+									до 12
+								</div>
+							</div>
+							<div className='card-body'>
+								<h5 className='card-catalog-title'>Новая сосновка</h5>{' '}
+								<p className='card-catalog-text'>
+									<img src={bed} alt='' /> 25 спальных мест
+								</p>
+								<p className='card-catalog-text'>
+									<img src={entertaiment} alt='' /> Настольный теннис
+								</p>
+								<p className='card-catalog-text'>
+									<img src={stroke} alt='' /> Бассейн
+								</p>
+								<p className='card-catalog-text'>
+									<img src={basseyn} alt='' /> Сауна{' '}
+								</p>
+								<div className='d-flex align-items-center justify-content-between mt-4'>
+									<p className='card-catalog-price m-0'>от 10 000 ₽ / сутки </p>
+									<a className='card-catalog-link' href='#'>
+										Подробнее
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className='col-12 col-sm-12 col-md-6 col-lg-4'>
+						<div className='catalog-card'>
+							<div className='position-relative'>
+								<img
+									src='https://domiktut.ru/wp-content/uploads/2022/07/20200827_113924-480x320.jpg'
+									className='card-img-top'
+									alt='Fissure in Sandstone'
+								/>
+								<div className='favorite'>
+									<img src={heart} alt='' />
+								</div>
+								<div className='number-of-poeple'>
+									<img src={people} alt='' />
+									до 12
+								</div>
+							</div>
+							<div className='card-body'>
+								<h5 className='card-catalog-title'>Новая сосновка</h5>{' '}
+								<p className='card-catalog-text'>
+									<img src={bed} alt='' /> 25 спальных мест
+								</p>
+								<p className='card-catalog-text'>
+									<img src={entertaiment} alt='' /> Настольный теннис
+								</p>
+								<p className='card-catalog-text'>
+									<img src={stroke} alt='' /> Бассейн
+								</p>
+								<p className='card-catalog-text'>
+									<img src={basseyn} alt='' /> Сауна{' '}
+								</p>
+								<div className='d-flex align-items-center justify-content-between mt-4'>
+									<p className='card-catalog-price m-0'>от 10 000 ₽ / сутки </p>
+									<a className='card-catalog-link' href='#'>
+										Подробнее
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className='col-12 col-sm-12 col-md-6 col-lg-4'>
+						<div className='catalog-card'>
+							<div className='position-relative'>
+								<img
+									src='https://domiktut.ru/wp-content/uploads/2022/07/20200827_113924-480x320.jpg'
+									className='card-img-top'
+									alt='Fissure in Sandstone'
+								/>
+								<div className='favorite'>
+									<img src={heart} alt='' />
+								</div>
+								<div className='number-of-poeple'>
+									<img src={people} alt='' />
+									до 12
+								</div>
+							</div>
+							<div className='card-body'>
+								<h5 className='card-catalog-title'>Новая сосновка</h5>{' '}
+								<p className='card-catalog-text'>
+									<img src={bed} alt='' /> 25 спальных мест
+								</p>
+								<p className='card-catalog-text'>
+									<img src={entertaiment} alt='' /> Настольный теннис
+								</p>
+								<p className='card-catalog-text'>
+									<img src={stroke} alt='' /> Бассейн
+								</p>
+								<p className='card-catalog-text'>
+									<img src={basseyn} alt='' /> Сауна{' '}
+								</p>
+								<div className='d-flex align-items-center justify-content-between mt-4'>
+									<p className='card-catalog-price m-0'>от 10 000 ₽ / сутки </p>
+									<a className='card-catalog-link' href='#'>
+										Подробнее
+									</a>
+								</div>
 							</div>
 						</div>
 					</div>
